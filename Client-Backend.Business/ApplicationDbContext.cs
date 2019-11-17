177380d1e8b57d127e33a80508d18e24b5e583c3
dumitrescuandrei <catalinandreidumitrescu@gmail.com>
2019-11-17 13:26:52 +0200

    added routes

---
+++ Client-Backend.Business/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Client_Backend;
+
+namespace Client_Backend.Business
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        /*public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityUser>().ToTable("user");
+            modelBuilder.Entity<User>().ToTable("user");
+
+            modelBuilder.Entity<IdentityRole>().ToTable("role");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("userrole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("userclaim");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("userlogin");
+        }
+        */
+    }
+}
---
+++ Client-Backend/Controllers/UserController/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Client_Backend.DataAccess;
+using Microsoft.AspNetCore.Identity;
+
+namespace Client_Backend.Controllers.User_Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private SignInManager<User> _signInManager;
+        private UserManager<User> _userManager;
+
+        public AuthController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _userManager = UserManager;
+            _signInManager = SignInManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserRegistration model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new User { 
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.UserName,
+                    EmailAddress = model.EmailAddress
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return StatusCode(202);
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return StatusCode(205);
+        }
+
+        [HttpGet("Login")]
+        public IActionResult Login(string ReturnUrl = "")
+        {
+            var model = new UserLogin { ReturnUrl = ReturnUrl };
+            return StatusCode(200); // "no content, refresh; refresh client view
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLogin model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, false);
+                if(result.Succeeded)
+                {
+                    return Ok();
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return StatusCode(403); // implicit failure, failed login, includes bad credentials/permissions
+        }
+    }
+}
